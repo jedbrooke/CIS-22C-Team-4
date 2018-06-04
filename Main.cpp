dd6aff21b95adca20e9bae46099328b8e5b2d02b
Mohamed-Elgharbawy <30507689+Mohamed-Elgharbawy@users.noreply.github.com>
2018-06-04 11:44:37 -0700

    Main Fix
    Deleted old main in branch "test," put this in new branch "main"

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Jun 4, 2018
+ *      Author: melgharbawy
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello Final Project!" << endl;
+
+	return 0;
+}
