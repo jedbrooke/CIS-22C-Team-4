6b1e5d6609205c46eb3ae1cd56758adb04173668
alexrao2000 <alexrao20002@gmail.com>
2018-06-23 15:04:24 -0700

    Add files via upload
    stuff

--- FileLoader.cpp
+++ FileLoader.cpp
@@ -14,36 +14,36 @@ BST<Product> FileLoader::loadProducts(string path){
 
 	cout << "loading products..." << endl;
 
-    BST<Product> products;
-    ifstream fis(path.c_str());
-    string line;
-    if (fis.is_open()) {
-        while (getline(fis, line)) {
-            string manf_company = line;
-            string model;
-            getline(fis, model);
-            double screen_size;
-            fis >> screen_size;
-            getline(fis, line); //advance to the next line
-            unsigned cpuGen;
-            fis >> cpuGen;
-            getline(fis, line); //advance to the next line
-            unsigned year;
-            fis >> year;
-            getline(fis, line); //advance to the next line
-            unsigned price;
-            fis >> price;
-            getline(fis, line); //advance to the next line
+	BST<Product> products;
+	ifstream fis(path.c_str());
+	string line;
+	if (fis.is_open()) {
+		while (getline(fis, line)) {
+			string manf_company = line;
+			string model;
+			getline(fis, model);
+			double screen_size;
+			fis >> screen_size;
+			getline(fis, line); //advance to the next line
+			unsigned cpuGen;
+			fis >> cpuGen;
+			getline(fis, line); //advance to the next line
+			unsigned year;
+			fis >> year;
+			getline(fis, line); //advance to the next line
+			unsigned price;
+			fis >> price;
+			getline(fis, line); //advance to the next line
 
-            Product p(manf_company,model,screen_size,cpuGen,year,price);
-            products.insert(p);
+			Product p(manf_company,model,screen_size,cpuGen,year,price);
+			products.insert(p);
 
-            getline(fis, line); //skip the emtpy line
+			getline(fis, line); //skip the emtpy line
 
-        }
-    }
-    fis.close();
-    return products;
+		}
+	}
+	fis.close();
+	return products;
 }
 
 HashTable<Customer> FileLoader::loadCustomers(string path) {
@@ -55,37 +55,37 @@ HashTable<Customer> FileLoader::loadCustomers(string path) {
 	string line;
 	if (fis.is_open()) {
 		while (getline(fis, line)) {
-            string username = line;
-            string password;
-            getline(fis, password);
-            string first_name;
-            fis >> first_name;
-            getline(fis, line); //advance to the next line
-            string last_name;
-            fis >> last_name;
-            getline(fis, line); //advance to the next line
-            bool isEmployee;
-            fis >> isEmployee;
-            getline(fis, line); //advance to the next line
-            string address;
-            fis >> address;
-            getline(fis, line); //advance to the next line
-            string city;
-            fis >> city;
-            getline(fis, line); //advance to the next line
-            unsigned zip;
-            fis >> zip;
-            getline(fis, line); //advance to the next line
-            string email;
-            fis >> email;
-            getline(fis, line); //advance to the next line
+			string username = line;
+			string password;
+			getline(fis, password);
+			string first_name;
+			fis >> first_name;
+			getline(fis, line); //advance to the next line
+			string last_name;
+			fis >> last_name;
+			getline(fis, line); //advance to the next line
+			bool isEmployee;
+			fis >> isEmployee;
+			getline(fis, line); //advance to the next line
+			string address;
+			fis >> address;
+			getline(fis, line); //advance to the next line
+			string city;
+			fis >> city;
+			getline(fis, line); //advance to the next line
+			unsigned zip;
+			fis >> zip;
+			getline(fis, line); //advance to the next line
+			string email;
+			fis >> email;
+			getline(fis, line); //advance to the next line
 
-            Customer c(username, password, first_name, last_name, isEmployee, address, city, zip, email);
-            customers.insert(c);
+			Customer *c = new Customer(username, password, first_name, last_name, isEmployee, address, city, zip, email);
+			customers.insert(*c);
 
-            getline(fis, line); //skip the emtpy line
+			getline(fis, line); //skip the emtpy line
 
-        }
+		}
 	}
 	fis.close();
 	return customers;
@@ -100,23 +100,24 @@ HashTable<Employee> FileLoader::loadEmployees(string path) {
 	string line;
 	if (fis.is_open()) {
 		while (getline(fis, line)) {
-            string username = line;
-            string password;
-            getline(fis, password);
-            string first_name;
-            fis >> first_name;
-            getline(fis, line); //advance to the next line
-            string last_name;
-            fis >> last_name;
-            getline(fis, line); //advance to the next line
-            bool isEmployee;
-            fis >> isEmployee;
-            getline(fis, line); //advance to the next line
+			string username = line;
+			string password;
+			getline(fis, password);
+			string first_name;
+			fis >> first_name;
+			getline(fis, line); //advance to the next line
+			string last_name;
+			fis >> last_name;
+			getline(fis, line); //advance to the next line
+			bool isEmployee;
+			fis >> isEmployee;
+			getline(fis, line); //advance to the next line
 
-            Employee e(username, password, first_name, last_name, isEmployee);
-            employees.insert(e);
+			cout << first_name;
+			Employee *e = new Employee(username, password, first_name, last_name, isEmployee);
+			employees.insert(*e);
 
-            getline(fis, line); //skip the emtpy line
+			getline(fis, line); //skip the emtpy line
 
 		}
 	}
